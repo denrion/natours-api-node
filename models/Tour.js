dcf982388038d40ca20d25402a5d3be2c4df4708
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-06-16 14:20:48 +0200

    added indecies to the Tour model

--- models/Tour.js
+++ models/Tour.js
@@ -107,6 +107,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // ************************ VIRTUALS ************************ //
 tourSchema.virtual('durationWeeks').get(function () {
   return Math.ceil(this.duration / 7);
