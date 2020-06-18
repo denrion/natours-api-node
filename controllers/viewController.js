53628d1849a59f58710aca330c46c27bda120e7f
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-06-18 11:56:55 +0200

    completed the tour overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,12 @@
-export const getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+import status from 'http-status';
+import Tour from '../models/Tour.js';
+import catchAsync from '../utils/catchAsync.js';
+
+export const getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  res.status(status.OK).render('overview', { title: 'All tours', tours });
+});
 
 export const getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
+  res.status(status.OK).render('tour', { title: 'The Forest Hiker Tour' });
 };
