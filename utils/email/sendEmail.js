da619a9fd1ef5327c0501dea8705617267ab261c
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-06-22 14:54:19 +0200

    buidling a complex email handler

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -1,23 +1,55 @@
+import htmlToText from 'html-to-text';
 import nodemailer from 'nodemailer';
+import path from 'path';
+import pug from 'pug';
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Natours ${process.env.EMAIL_FROM}`;
+  }
 
-  const mailOptions = {
-    from: 'Natours <natours@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
 
-  await transporter.sendMail(mailOptions);
-};
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-export default sendEmail;
+  async send(template, subject) {
+    // 1) Render HTML based on pug template
+    const html = pug.renderFile(
+      `${path.resolve('views', 'emails', `${template}.pug`)}`,
+      { firstName: this.firstName, url: this.ur, subject }
+    );
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+}
+
+export default Email;
