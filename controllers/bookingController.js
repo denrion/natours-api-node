379f3b80d4d4beb830660831c0b9bfb36983ecd5
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-06-23 13:12:27 +0200

    implemented temporary booking creation after stripe payment success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,10 +25,15 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
   if (!tour)
     return next(new NotFoundError('No tour found with the specified id'));
 
+  // will be changed after the site is deployed
+  const temporarySuccessUrl = `${req.protocol}://${req.get('host')}/?tour=${
+    req.params.tourId
+  }&user=${req.user.id}&price=${tour.price}`;
+
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: temporarySuccessUrl,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -49,6 +54,18 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
     status: ResponseStatus.SUCCESS,
     session,
   });
+});
+
+export const createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is ony TEMPORARY, because it`s NOT SECURE becuase
+  // everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
 
 // @desc      Get All Bookings
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { createBookingCheckout } from '../controllers/bookingController.js';
 import {
   getAccount,
   getLoginForm,
@@ -9,7 +10,7 @@ import isAuth, { isLoggedIn } from '../middleware/isAuth.js';
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', isAuth, getAccount);
